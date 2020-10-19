330079133f3664785bfc3be0f69a775266a1fe4f
HebleV <visheble@yahoo.com>
2020-10-19 22:23:02 +0530

    Testing basic data types

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+function add(num1, num2, showResult, phrase) {
+    if (showResult) {
+        var total = num1 + num2;
+        console.log(phrase + total);
+    }
+    else {
+        return num1 + num2;
+    }
+}
+var printResult = true;
+var resultPhrase = 'Result is ';
+add(1, 2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('hello world');
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+    if (showResult) {
+        const total = num1 + num2;
+        console.log(phrase + total)
+    } else {
+        return num1 + num2;
+    }
+
+}
+
+const printResult = true;
+const resultPhrase = 'Result is ';
+
+add(1, 2, printResult, resultPhrase);
